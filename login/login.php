deb2067d18a1f04eaf5df88821ad2d0c32ff8758
ren1023 <renyou.1023@gmail.com>
2023-10-28 00:37:01 +0800

    login.php 的style修改

--- login/login.php
+++ login/login.php
@@ -6,11 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>會員登入</title>
     <style>
-        .login-block {}
+        .login-block {
+            /* display:flex;
+            justify-content: center;
+            align-items: baseline; */
+            padding: 30px 40px;
+            border: 2px solid #999;
+            box-shadow: 2px 2px 15px #999;
+            border-radius: 10px;
+            margin: 10px auto;
+            width:280px;
 
-        .login-input {}
+        }
 
-        .btn input[type='input'] {}
+        .login-input {
+            margin: 10px;
+
+        }
+
+        .login-input input[type='text']{
+            border: 1px solid blue;
+            /* margin-bottom: 20px; */
+        }
+
+        .login-input input,.login-input label{
+            font-size: 20px;
+            /* margin-bottom: 20px; */
+            padding: 5px;
+        }
+
+
+
+        .btn{
+            text-align: center;
+            margin-top: 15px;
+        }
+
+        .btn input[type='submit'],
+        input[type='reset'] {
+            padding: 5px 10px;
+            border: 1px solid green;
+            border-radius: 5px;
+            margin: 5px;
+
+        }
+
     </style>
 </head>
 
@@ -31,7 +71,7 @@
                 <label for="acc">帳號：</label>
                 <input type="text" name="acc" id="acc">
             </div>
-            <div>
+            <div class="login-input">
                 <label for="pw">密碼：</label>
                 <input type="text" name="pw" id="pw">
             </div>
