a84923efc3b7e23ebb8bcc4480c4bcb4e6334af3
ren1023 <renyou.1023@gmail.com>
2023-11-13 16:17:32 +0800

    新增登入資料

--- 20231113_member/add_user.php
+++ 20231113_member/add_user.php
@@ -1,10 +1,16 @@
 <?php
 $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
+// 使用 trim 函數刪除用戶輸入數據的開頭和結尾的空格（以確保數據的一致性）
+// 使用 htmlspecialchars 函數對用戶輸入數據進行 HTML 轉義，以防止跨站腳本攻擊（XSS 攻擊）
+$acc=htmlspecialchars(trim($_POST['acc']));
+
+
 
 $sql = "insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
-                     values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
+                     values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
 $pdo-> exec($sql);
 
 header("Location:./index.php");
+
 ?>
--- 20231113_member/reg.php
+++ 20231113_member/reg.php
@@ -5,41 +5,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>會員註冊</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </head>
 
-<body>
-    <h2>會員註冊</h2>
-    <form action="./add_user.php" method="post">
-        <div>
-            <label>帳號：</label>
-            <input type="text" name="acc" id="acc">
-        </div>
-        <div>
-            <label>密碼：</label>
-            <input type="password" name="pw" id="pw">
-        </div>
-        <div>
-            <label>姓名：</label>
-            <input type="text" name="name" id="name">
-        </div>
-        <div>
-            <label>電子郵件：</label>
-            <input type="text" name="email" id="email">
-        </div>
+<div id=app class="container">
 
-        <div>
-            <label>居住地：</label>
-            <input type="text" name="address" id="address">
-        </div>
-
-        <div>
-            <input type="submit" value="送出">
-            <input type="reset" value="重置">
-        </div>
-
-
-    </form>
-
-</body>
+    <body>
+        <h2>會員註冊</h2>
+        <form  action="./add_user.php" method="post">
+            <div class="input-group mb-3">
+                <label class=" input-group-text">帳號：</label>
+                <input  class="form-control"  type="text" name="acc" id="acc">
+            </div>
+            <div class="input-group mb-3">
+                <label class=" input-group-text">密碼：</label>
+                <input class="form-control" type="password" name="pw" id="pw">
+            </div>
+            <div class="input-group mb-3">
+                <label class=" input-group-text">姓名：</label>
+                <input class="form-control" type="text" name="name" id="name">
+            </div>
+            <div class="input-group mb-3">
+                <label class=" input-group-text">電子郵件：</label>
+                <input class="form-control" type="text" name="email" id="email">
+            </div>
+            <div class="input-group mb-3">
+                <label class=" input-group-text">居住地：</label>
+                <input  class="form-control" type="text" name="address" id="address">
+            </div>
+            <div class="input-group mb-3">
+                <input type="submit" value="送出">
+                <input type="reset" value="重置">
+            </div>
+        </form>
+    </body>
+</div>
 
 </html>
